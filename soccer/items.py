2b53634e9ec1f7f34aedb9d63ff5a1d0853c8ec4
wlsnx <wanlisnx@gmail.com>
2015-03-20 15:44:56 +0800

    rebuild cs crawler

---
+++ soccer/items.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+from scrapy.contrib.loader.processor import Compose, TakeFirst
+
+
+def try_trans_int(s):
+    try:
+        return int(s)
+    except ValueError:
+        return 0
+
+
+class DefaultValueItem(scrapy.Item):
+
+    def __getitem__(self, key):
+        try:
+            return self._values[key]
+        except KeyError:
+            field = self.fields[key]
+            if "default" in field:
+                return field["default"]
+            raise
+
+
+class Football(DefaultValueItem):
+    home_shot            = scrapy.Field(default=0 , output_processor=TakeFirst())
+    away_shot            = scrapy.Field(default=0 , output_processor=TakeFirst())
+    home_shots           = scrapy.Field(default=0 , output_processor=TakeFirst())
+    away_shots           = scrapy.Field(default=0 , output_processor=TakeFirst())
+    home_fouls           = scrapy.Field(default=0 , output_processor=TakeFirst())
+    away_fouls           = scrapy.Field(default=0 , output_processor=TakeFirst())
+    home_corner          = scrapy.Field(default=0 , output_processor=TakeFirst())
+    away_corner          = scrapy.Field(default=0 , output_processor=TakeFirst())
+    home_offside         = scrapy.Field(default=0 , output_processor=TakeFirst())
+    away_offside         = scrapy.Field(default=0 , output_processor=TakeFirst())
+    home_ball_possession = scrapy.Field(default=0 , output_processor=TakeFirst())
+    away_ball_possession = scrapy.Field(default=0 , output_processor=TakeFirst())
+    home_yellow_card     = scrapy.Field(default=0 , output_processor=TakeFirst())
+    away_yellow_card     = scrapy.Field(default=0 , output_processor=TakeFirst())
+    home_red_card        = scrapy.Field(default=0 , output_processor=TakeFirst())
+    away_red_card        = scrapy.Field(default=0 , output_processor=TakeFirst())
+    home_saving          = scrapy.Field(default=0 , output_processor=TakeFirst())
+    away_saving          = scrapy.Field(default=0 , output_processor=TakeFirst())
+    home_scores          = scrapy.Field(default=0 , iutput_processor=Compose(try_trans_int) , output_processor=TakeFirst())
+    away_scores          = scrapy.Field(default=0 , iutput_processor=Compose(try_trans_int) , output_processor=TakeFirst())
+    date                 = scrapy.Field()
+    time                 = scrapy.Field(output_processor=TakeFirst())
+    attendance           = scrapy.Field(default=0)
+    mvp                  = scrapy.Field(default="")
+    mvp_id               = scrapy.Field(default=0)
+    mid                  = scrapy.Field(default=0, output_processor=TakeFirst())
+
+
+class FootballDetail(DefaultValueItem):
+    mid             = scrapy.Field(default = 0  , output_processor = TakeFirst())
+    min             = scrapy.Field(default = 0  , output_processor = TakeFirst())
+    team            = scrapy.Field(default = 1  , output_processor = TakeFirst())
+    type            = scrapy.Field(default = 1  , output_processor = TakeFirst())
+    player_a        = scrapy.Field(default = "" , output_processor = TakeFirst())
+    player_a_id     = scrapy.Field(default = 0  , output_processor = TakeFirst())
+    player_a_domain = scrapy.Field(default = "" , output_processor = TakeFirst())
+    player_b        = scrapy.Field(default = "" , output_processor = TakeFirst())
+    player_b_id     = scrapy.Field(default = 0  , output_processor = TakeFirst())
+    player_b_domain = scrapy.Field(default = "" , output_processor = TakeFirst())
+
+
+class Match(DefaultValueItem):
+    id          = scrapy.Field(default = 0, output_processor = TakeFirst())
+    m_time      = scrapy.Field(output_processor=TakeFirst())
+    home_scores = scrapy.Field(default = 0, iutput_processor   = Compose(try_trans_int), output_processor = TakeFirst())
+    away_scores = scrapy.Field(default = 0, iutput_processor = Compose(try_trans_int), output_processor = TakeFirst())
+    finish      = scrapy.Field(default = 0, output_processor = TakeFirst())
+
---
+++ soccer/pipelines.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+import redis
+from soccer.items import Match, Football, FootballDetail
+import cPickle as pickle
+
+
+def equal(item, old_item):
+    for key, value in item.items():
+        if not (key in old_item and old_item[key] == value):
+            return False
+    return True
+
+def player_info(table, player_name):
+    player = table.find_one(player_name=player_name)
+    yield player["id"] if player else 0
+    yield player["domain"] if player else ""
+
+
+class CachedDatabasePipeline(object):
+
+    def __init__(self):
+        self.cache = redis.StrictRedis()
+        self.db = None
+
+    def process_item(self, item, spider):
+        self.db = spider.db
+        if isinstance(item, Match):
+            key = "match:{0}".format(item["id"])
+        elif isinstance(item, Football):
+            key = "football:{0}".format(item["mid"])
+        elif isinstance(item, FootballDetail):
+            key = "footballdetail:{mid}:{min}:{team}:{type}".format(**item)
+
+        old_item = self.cache.get(key)
+        if not (old_item and equal(item, pickle.loads(old_item))):
+            self.cache.set(key, pickle.dumps(item), ex=2 * 3600)
+            self.save(item)
+        return item
+
+    def save(self, item):
+        if isinstance(item, Match):
+            table = self.db.get_table("match")
+            table.update(item, ["id"])
+
+        elif isinstance(item, Football):
+            table = self.db.get_table("match_football")
+            match = table.find_one(mid=item["mid"])
+            if match:
+                table.update(item, ["mid"])
+            else:
+                match_table = self.db.get_table("match")
+                match_a = match_table.find_one(id=item["mid"])
+                item["date"] = match_a["date"]
+                item["time"] = match_a["time"]
+                table.insert(item)
+
+        elif isinstance(item, FootballDetail):
+            table = self.db.get_table("match_football_details")
+            player_table = self.db.get_table("player")
+            player_a_id, player_a_domain = player_info(player_table, item["player_a"])
+            player_b_id, player_b_domain = player_info(player_table, item["player_b"])
+            item["player_a_id"] = player_a_id
+            item["player_a_domain"] = player_a_domain
+            item["player_b_id"] = player_b_id
+            item["player_b_domain"] = player_b_domain
+
+            match = table.find_one(**item)
+            if not match:
+                table.insert(item)
+
